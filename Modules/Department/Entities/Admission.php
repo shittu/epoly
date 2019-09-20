353f4334e0a082e6bd7b46945b3a686d52c17798
Abu Saleem <abusaleem>
2019-09-20 16:21:28 +0100

    create student admission related database

---
+++ Modules/Department/Entities/Admission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Department\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Admission extends Model
+{
+    protected $fillable = [];
+}
---
+++ Modules/Student/Entities/StudentAccount.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Student\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StudentAccount extends Model
+{
+    protected $fillable = [];
+}
