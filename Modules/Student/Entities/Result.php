74ea0a79739bacd8abe688dc9474b078aa4ed965
Abu Saleem <abusaleem>
2019-09-25 19:19:36 +0100

    create two method that will compute the grade and points of the result and save on the result model

--- Modules/Student/Entities/Result.php
+++ Modules/Student/Entities/Result.php
@@ -17,4 +17,42 @@ class Result extends BaseModel
     	return $this->hasMany(ResultRemark::class);
     }
 
+    public function point()
+    {
+    	switch ($this->grade) {
+    		case 'A':
+    			$point = 4.00;
+    			break;
+    		case 'AB':
+    			$point = 3.75;
+    			break;
+    		
+    		case 'B':
+    			$point = 3.50;
+    			break;
+    		
+    		case 'BC':
+    			$point = 3.25;
+    			break;
+    		
+    		case 'C':
+    			$point = 3.00;
+    			break;
+    		
+    		case 'CD':
+    			$point = 2.75;
+    			break;
+    		case 'D':
+    			$point = 2.50;
+    			break;
+    		case 'E':
+    			$point = 2.00;
+    			break;
+    		default:
+    			$point = 0.00;
+    			break;
+    	}
+    	return $point;
+    }
+
 }
