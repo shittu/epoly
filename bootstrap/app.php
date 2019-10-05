d10db21bcf5b3e7e841befa9bd407d83634cd65d
Abu Saleem <abusaleem>
2019-10-05 15:04:03 +0100

    i want to delete my vendor folder

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 $app->register(\Wnx\LaravelStats\StatsServiceProvider::class);
+
+$app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
 /*
 |--------------------------------------------------------------------------
 | Return The Application
