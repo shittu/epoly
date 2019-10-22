09cb3e10c0973e636a2076ca1aa93ccac7573428
Abu Saleem <abusaleem>
2019-10-22 18:18:16 +0100

    create examOfficer helper method

--- resources/views/include/menu/lecturer/menu.blade.php
+++ resources/views/include/menu/lecturer/menu.blade.php
@@ -1,8 +1,30 @@
 <li>
     <a href="{{route('lecturer.courses.index')}}">My Courses</a>
 </li>
+@if(examOfficer())
 <li>
-    <a href="#">Results</a>
+    <a href="#">Department Results</a>
+    <ul>
+        <li>
+            <a href="{{route('department.student.result.index')}}">Check Student Results</a>
+        </li>
+        <li>
+            <a href="#">Results Statistics</a>
+        </li>
+        <li>
+            <a href="{{route('department.result.course.index')}}">View Result</a>
+        </li>
+        <li>
+            <a href="{{route('department.result.course.bating.index')}}">AB Format</a>
+        </li>
+        <li>
+            <a href="{{route('department.result.remark.index')}}">Remarks</a>
+        </li>
+    </ul>
+</li>
+@endif
+<li>
+    <a href="#">My Results</a>
     <ul>
         <li>
         	<a href="{{route('lecturer.result.templete.index')}}">Download Result Templete</a>
